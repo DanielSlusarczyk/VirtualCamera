f3bf2805f9baf08b587b7160f4545c8f972648a1
Daniel <daniel00150015@gmail.com>
2023-04-03 19:25:40 +0200

    First movement support

--- camera/src/main/java/app/Main.java
+++ camera/src/main/java/app/Main.java
@@ -1,12 +1,10 @@
 package app;
 
 import app.configuration.Configuration;
-import app.geometry.Edge;
 import app.objects.Prism;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
-import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
 
@@ -17,17 +15,25 @@ public class Main extends Application implements Configuration {
     public void start(Stage stage) {
         
         Pane pane = new Pane();
-
         Prism prism = new Prism();
-        for (Edge l : prism.rotateOY(30).rotateOX(45).project().getEdges()){
-
-            Line line = new Line(l.getA().getX(), l.getA().getY(), l.getB().getX(), l.getB().getY());
-
-            pane.getChildren().add(line);
-        }
-
         Scene scene = new Scene(pane, WINDOW_WIDTH, WINDOW_HEIGHT);
 
+        scene.setOnKeyPressed(event -> {
+            pane.getChildren().clear();
+            switch(event.getCode()) {
+                case A:
+                    prism.rotateOX(1.0);
+                    break;
+                case D:
+                    prism.rotateOX(-1.0);
+                    break;
+                default:
+                    break;
+            }
+            pane.getChildren().addAll(prism.getLines());
+            stage.setScene(scene);
+        });
+        
         stage.setScene(scene);
         stage.show();
     }
