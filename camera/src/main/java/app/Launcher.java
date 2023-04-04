b3c2e9eddeb8fc28459cfad7c62cdebf1e141195
Daniel <daniel00150015@gmail.com>
2023-04-04 21:46:16 +0200

    Added support for simultaneously pressed buttons

--- camera/src/main/java/app/Launcher.java
+++ camera/src/main/java/app/Launcher.java
@@ -2,6 +2,6 @@ package app;
 
 public class Launcher {
     public static void main(String[] args) {
-        Main.main(args);
+        Controller.run(args);
     }
 }
--- camera/src/main/java/app/config/Configuration.java
+++ camera/src/main/java/app/config/Configuration.java
@@ -1,10 +1,21 @@
 package app.config;
 
+import javafx.scene.input.KeyCode;
+
 public interface Configuration {
     double WINDOW_WIDTH = 640;
     double WINDOW_HEIGHT = 640;
     double MOVE_INC = 1.0;
-    double ANGLE_INC = 1.0;   
+    double ANGLE_INC = 1.0;
+
+    final KeyCode LEFT_MOVE = KeyCode.A;
+    KeyCode RIGHT_MOVE = KeyCode.D;
+    KeyCode FORWARD_MOVE = KeyCode.W;
+    KeyCode BACKWARD_MOVE = KeyCode.S;
+    KeyCode LEFT_ROTATION = KeyCode.LEFT;
+    KeyCode RIGHT_ROTATION = KeyCode.RIGHT;
+    KeyCode UP_ROTATION = KeyCode.UP;
+    KeyCode DOWN_ROTATION = KeyCode.DOWN;
 
     double HALF_WINDOW_WIDTH = WINDOW_WIDTH / 2;
     double HALF_WINDOW_HEIGHT = WINDOW_HEIGHT / 2;
